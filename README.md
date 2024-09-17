2749ff2ac4c72c82c03b81e15e7004eac2b774a7
Nur <hanako.yugiamane101@gmail.com>
2024-09-18 01:32:31 +0600

    Wrote README file, made mobile responsive

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
-# React + Vite
+# KnowledgeCafe
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+A simple and elegant blog site created with React. Showcasing some quality blogs.
 
-Currently, two official plugins are available:
+# Features
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- Track down the time you spent on reading a blog by click on the **"Mark as read"** button.
+- Save your favorite blogs with the **"Bookmark"** button.
+- Mobile Responsive
+
+# Technologies
+
+- React
+- HTML
+- CSS
+
+# Live Link
+
+[KnowledgeCafe](https://knowledge-cafe-han.surge.sh/)
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,27 @@
-import './Header.css'
+import "./Header.css";
 
-import profile from '../../assets/images/profile.png'
+import profile from "../../assets/images/profile.png";
 
 const Header = () => {
-    return (
-        <nav className='navbar'>
-            <div>
-                <h1>KnowledgeCafe</h1>
-            </div>
-            <div className='nav-links'>
-                <a href="/home">Home</a>
-                <a href="/blog">Blog</a>
-                <a href="/about">About</a>
-                <a href='/profile'>
-                    <img src={profile}/>
-                </a>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <div className="navbar-info">
+        <div>
+          <h1 className="site-name">KnowledgeCafe</h1>
+        </div>
+        <div className="nav-links">
+          <a href="/home">Home</a>
+          <a href="/blog">Blog</a>
+          <a href="/about">About</a>
+        </div>
+      </div>
+      <div>
+        <a href="/profile" className="profile-image">
+          <img src={profile} />
+        </a>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
